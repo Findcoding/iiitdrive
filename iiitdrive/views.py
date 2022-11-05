f4e0b85f4bcec8e7fab46b9df74f0eba93b601f3
findcoding <bijendarprasad772244@gmail.com>
2022-11-05 15:01:49 +0530

    view.py updated

--- iiitdrive/views.py
+++ iiitdrive/views.py
@@ -1,3 +1,15 @@
+
+from urllib import request
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import redirect, render
+from django.urls import reverse
 from django.shortcuts import render
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Create your views here.
+
+
+def homepage(request):
+    print("hello world")
+    return render(request, 'index.html')
