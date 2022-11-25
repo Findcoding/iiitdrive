140f38e66a248a33a77543210c1771ab7a56ee0d
findcoding <bijendarprasad772244@gmail.com>
2022-11-25 21:46:24 +0530

    API is set for data & time

--- static/js/home.js
+++ static/js/home.js
@@ -51,23 +51,44 @@ window.addEventListener("pageshow", function(event) {
 });
 
 
+const date_url = "http://worldtimeapi.org/api/timezone/Asia/Kolkata";
 
-function refreshTime() {
-    var date = new Date();
-    var hour = date.toLocaleString("default", { hour: "2-digit" });
-    const myArray = hour.split(" ");
-    var minute = date.toLocaleString("default", { minute: "2-digit" });
-    var second = date.toLocaleString("default", { second: "2-digit" });
+async function refreshTime() {
 
-    document.getElementById("hour").textContent = myArray[0] + ":";
-    document.getElementById("min").textContent = minute + ":";
-    document.getElementById("sec").textContent = second + " ";
-    document.getElementById("zone").textContent = myArray[1];
+    try {
+        const response = await fetch(date_url);
+        const data = await response.json();
 
-    var datetime = date.toDateString();
-    const arr = datetime.split(" ");
-    document.getElementById("date").textContent = arr[0] + ", " + arr[2] + " " + arr[1] + " " + arr[3];
+        const dateTimeInParts = data.datetime.split("T");
+
+        const t = new Date(dateTimeInParts);
+        // console.log(t);
+        // const date = dateTimeInParts[0];
+        // const time = dateTimeInParts[1];
+
+        var date = new Date(dateTimeInParts);
+        // console.log(date);
+        var hour = date.toLocaleString("default", { hour: "2-digit" });
+        const myArray = hour.split(" ");
+        var minute = date.toLocaleString("default", { minute: "2-digit" });
+        var second = date.toLocaleString("default", { second: "2-digit" });
+
+        document.getElementById("hour").textContent = myArray[0] + ":";
+        document.getElementById("min").textContent = minute + ":";
+        document.getElementById("sec").textContent = second + " ";
+        document.getElementById("zone").textContent = myArray[1];
+
+        var datetime = date.toDateString();
+        const arr = datetime.split(" ");
+        document.getElementById("date").textContent = arr[0] + ", " + arr[2] + " " + arr[1] + " " + arr[3];
+
+
+    } catch (err) {
+        alert("Something went wrong!! Please reload this page...");
+        location.reload();
+    }
 
 }
     
 setInterval(refreshTime, 1000);
+
