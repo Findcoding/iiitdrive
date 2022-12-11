bc18c7343c82d5007041c98cabb1331b6389ba84
findcoding <bijendarprasad772244@gmail.com>
2022-12-11 12:06:18 +0530

    login page updated

--- static/js/login.js
+++ static/js/login.js
@@ -9,3 +9,31 @@ toggle = () => {
 setTimeout(() => {
 	container.classList.add('sign-in');
 }, 200)
+
+
+
+function show() {
+    var password = document.getElementById("password");
+    var icon = document.querySelector(".fas");
+
+    // ========== Checking type of password ===========
+    if (password.type === "password") {
+        password.type = "text";
+    } else {
+        password.type = "password";
+    }
+}
+
+
+function Validate() {
+    var password = document.getElementById("psw").value;
+    var confirmPassword = document.getElementById("confirm_psw").value;
+    if (password != confirmPassword) {
+        alert("Password do not match.");
+        return false;
+    }
+    var sign_up = document.getElementById("sign_up");
+    alert("Account successfully created.");
+    sign_up.type = "submit"
+    return true;
+}
