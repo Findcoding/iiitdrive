1e2b42abb73e7730bb3f1983e82c1b71995ae5f5
findcoding <bijendarprasad772244@gmail.com>
2022-12-11 17:37:50 +0530

    Django login page modified

--- static/js/login.js
+++ static/js/login.js
@@ -16,7 +16,6 @@ function show() {
     var password = document.getElementById("id_password");
     var icon = document.querySelector(".fas");
 
-    // ========== Checking type of password ===========
     if (password.type === "password") {
         password.type = "text";
     } else {
@@ -24,16 +23,3 @@ function show() {
     }
 }
 
-
-function Validate() {
-    var password = document.getElementById("psw").value;
-    var confirmPassword = document.getElementById("confirm_psw").value;
-    if (password != confirmPassword) {
-        alert("Password do not match.");
-        return false;
-    }
-    var sign_up = document.getElementById("sign_up");
-    alert("Account successfully created.");
-    sign_up.type = "submit"
-    return true;
-}
