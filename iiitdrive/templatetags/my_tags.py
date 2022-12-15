dfc9694670a4a0d27aa0ba533dfbe326b64c1500
Abhimanyu Gupta <guptaabhimanyu23@gmail.com>
2022-12-16 04:08:09 +0530

    create get_trending_tags filter

--- iiitdrive/templatetags/my_tags.py
+++ iiitdrive/templatetags/my_tags.py
@@ -1,4 +1,7 @@
 from django import template
+from django.utils import timezone
+from django.db import models
+from ..models import Tag
 
 register = template.Library()
 
@@ -8,3 +11,10 @@ def get_social_link(socials, name) :
 		return socials.get(name = name).link
 	else:
 		return None
+
+@register.filter
+def get_trending_tags(trend_hours=5, trend_values=3) :
+	time_threshold_t = timezone.now() - timezone.timedelta(hours=trend_hours)
+	trend_tags = Tag.objects.filter(posts__created_at__lt = time_threshold_t).values('name').annotate(count = models.Count('posts')).order_by('-count')[:trend_values].values_list('name', flat = True)
+
+	return trend_tags
