f42e761b85e47a1919d471175c5b63f2b699ce3e
Findcoding <bijendar@ip-172-31-19-84.ap-southeast-2.compute.internal>
2022-12-20 12:07:05 +0000

    fix get_trending_tags and create is_liked_by_user

--- iiitdrive/templatetags/my_tags.py
+++ iiitdrive/templatetags/my_tags.py
@@ -15,6 +15,12 @@ def get_social_link(socials, name) :
 @register.filter
 def get_trending_tags(trend_hours=5, trend_values=3) :
 	time_threshold_t = timezone.now() - timezone.timedelta(hours=trend_hours)
-	trend_tags = Tag.objects.filter(posts__created_at__lt = time_threshold_t).values('name').annotate(count = models.Count('posts')).order_by('-count')[:trend_values].values_list('name', flat = True)
+	trend_tags = Tag.objects.filter(posts__created_at__gte = time_threshold_t).values('name').annotate(count = models.Count('posts')).order_by('-count')[:trend_values].values_list('name', flat = True)
 
 	return trend_tags
+
+
+
+@register.filter
+def is_liked_by_user(post, user):
+	return post.likes.filter(user=user).exists()
